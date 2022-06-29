151ec6567bb229bff124fa57253cb5b1ef7ee4a0
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-29 17:06:58 -0400

    Implement the `GenericModal.js` component in the `App.js` component.  We need to manage the Modal's state, so this is the right place to do it.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import { useState } from 'react';
 import InputContainer from './components/Inputs/InputContainer';
 import OutputContainer from './components/Outputs/OutputContainer';
+import GenericModal from './components/Modals/GenericModal';
 
 function App() {
   const [records, setRecords] = useState([]);
   const [id, setId] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
 
   const saveDataHandler = data => {
     setId(prevId => prevId + 1);
@@ -18,10 +21,21 @@ function App() {
     });
   };
 
+  const closeModalHandler = () => setShowModal(false);
+  const showModalHandler = message => {
+    setModalMessage(message);
+    setShowModal(true);
+  };
+
   return (
     <div className='App'>
-      <InputContainer onSave={saveDataHandler} />
+      <InputContainer onSave={saveDataHandler} onShowModal={showModalHandler} />
       <OutputContainer recordCount={records.length} records={records} />
+      <GenericModal
+        show={showModal}
+        onClose={closeModalHandler}
+        message={modalMessage}
+      />
     </div>
   );
 }
